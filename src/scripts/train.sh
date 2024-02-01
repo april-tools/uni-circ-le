d72dd7295f9b7ef3a924a793ed08b99d7b5099a8
anton <grasarade@gmail.com>
2024-02-01 18:12:55 +0100

    Added training command (example).

---
+++ src/scripts/train.sh
@@ -0,0 +1,4 @@
+python src/exp/train_main.py --dataset "mnist" \
+    --model-dir "experiments/temp" \
+    --lr 0.1 --num-sums 8 --num-input 8  --rg "QT" --layer "cp" \
+    --batch-size 100 --max-num-epochs 100
