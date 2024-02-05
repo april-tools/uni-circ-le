e7da07977b67a86bb3989ac0c7f4029a2e54caba
gengala <gennarogala1396@gmail.com>
2024-02-05 18:02:25 +0100

    minor

--- src/training.py
+++ src/training.py
@@ -363,4 +363,6 @@ if __name__ == "__main__":
         verbose=ARGS.progressbar
     )
 
-    print(eval_bpd(pc, test_x))
+    print('train bpd: ', eval_bpd(pc, train_x))
+    print('valid bpd: ', eval_bpd(pc, valid_x))
+    print('test  bpd: ', eval_bpd(pc, test_x))
