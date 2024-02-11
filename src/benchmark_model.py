b1456daefa688d4afda9d558f40c80de09cc0889
gengala <gennarogala1396@gmail.com>
2024-02-11 12:19:28 +0100

    improved code

--- src/benchmark_model.py
+++ src/benchmark_model.py
@@ -46,16 +46,12 @@ def timer(fn: Callable[[], T]) -> Tuple[T, float]:
     Returns:
         Tuple[T, float]: The original return value, and time in ms.
     """
-    # TODO: torch typing issue
     start_event = torch.cuda.Event(enable_timing=True)  # type: ignore[no-untyped-call]
     end_event = torch.cuda.Event(enable_timing=True)  # type: ignore[no-untyped-call]
     start_event.record(torch.cuda.current_stream())  # type: ignore[no-untyped-call]
-
     ret = fn()
-
     end_event.record(torch.cuda.current_stream())  # type: ignore[no-untyped-call]
     torch.cuda.synchronize()  # wait for event to be recorded
-    # TODO: Event.elapsed_time is not typed
     elapsed_time: float = start_event.elapsed_time(end_event)  # type: ignore[no-untyped-call]
     return ret, elapsed_time
 
@@ -71,66 +67,17 @@ def benchmarker(fn: Callable[[], T], device: torch.device) -> Tuple[T, Tuple[flo
     """
     torch.cuda.synchronize()  # finish all previous work before resetting mem stats
     torch.cuda.reset_peak_memory_stats()
-
     ret, elapsed_time = timer(fn)
-
-    peak_memory = torch.cuda.max_memory_allocated(device=device) / 2**20
+    peak_memory = torch.cuda.max_memory_allocated(device=device) / 1024 ** 3
     return ret, (elapsed_time, peak_memory)
-
-
-
-
-# device: torch.device("cuda")
-
-
-class _Modes(str, enum.Enum):  # TODO: StrEnum introduced in 3.11
-    """Execution modes."""
-
-    TRAIN = "train"
-    EVAL = "eval"
-
-
-@dataclass
-class _ArgsNamespace(argparse.Namespace):
-    mode: _Modes = _Modes.TRAIN
-    seed: int = 42
-    dataset: str = ""
-    num_batches: int = 20
-    batch_size: int = 128
-    region_graph: str = ""
-    layer: str = ""
-    num_latents: int = 32  # TODO: rename this
-    first_pass_only: bool = False
-    gpu: int = 0
-    results_csv: str = ""
-
-
-def process_args() -> _ArgsNamespace:
-    """Process command line arguments.
-
-    Returns:
-        ArgsNamespace: Parsed args.
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--mode", type=_Modes, choices=_Modes.__members__.values(), help="mode")
-    parser.add_argument("--seed", type=int, help="seed, 0 for disable")
-    parser.add_argument("--dataset", type=str, help="Unused, required for grid experiment")
-    parser.add_argument("--num_batches", type=int, help="num_batches")
-    parser.add_argument("--batch_size", type=int, help="batch_size")
-    parser.add_argument("--region_graph", type=str, help="region_graph to use")
-    parser.add_argument("--layer", type=str, help="Either 'cp', 'cp-shared' or 'tucker'")
-    parser.add_argument("--num_latents", type=int, help="num_latents")
-    parser.add_argument("--first_pass_only", action="store_true", help="first_pass_only")
-    parser.add_argument("--gpu", type=int, help="Which gpu to use")
-    parser.add_argument("--results-csv", type=str, help="Path of the results csv (will be created if inexistent)")
-    return parser.parse_args(namespace=_ArgsNamespace())
-
 
 
 
 @torch.no_grad()
 def evaluate(
-    pc: TensorizedPC, data_loader: DataLoader[Tuple[Tensor, ...]], device
+    pc: TensorizedPC,
+    data_loader: DataLoader[Tuple[Tensor, ...]],
+    device
 ) -> Tuple[Tuple[List[float], List[float]], float]:
     """Evaluate circuit on given data.
 
@@ -160,7 +107,10 @@ def evaluate(
 
 
 def train(
-    pc: TensorizedPC, optimizer: optim.Optimizer, data_loader: DataLoader[Tuple[Tensor, ...]], device
+    pc: TensorizedPC,
+    optimizer: optim.Optimizer,
+    data_loader: DataLoader[Tuple[Tensor, ...]],
+    device
 ) -> Tuple[Tuple[List[float], List[float]], float]:
     """Train circuit on given data.
 
@@ -195,23 +145,52 @@ def train(
     return (ts, ms), ll_total / len(data_loader)
 
 
-def do_benchmarking(args, mode: _Modes) -> None:
+def do_benchmarking(args, mode: str) -> None:
     """Execute the main procedure."""
 
-    assert args.region_graph, "Must provide a RG filename"
-    if args.gpu >= 0:
-        device = torch.device(f"cuda:{args.gpu}")
-    else:
-        device = torch.device("cpu")
-    print(args)
+    device = torch.device(f"cuda:{args.gpu}") if args.gpu >= 0 else torch.device("cpu")
 
-    if args.seed:
-        # TODO: find a way to set w/o with
-        RandomCtx(args.seed).__enter__()  # pylint: disable=unnecessary-dunder-call
-        set_determinism(check_hash_seed=False)
+    if mode == "train":
+        optimizer = optim.Adam(pc.parameters())  # just keep everything default
+        (ts, ms), ll_train = train(pc, optimizer, data_loader, device=device)
+        print("Train LL:", ll_train)
+    elif mode == "test":
+        (ts, ms), ll_eval = evaluate(pc, data_loader, device=device)
+        print("Evaluation LL:", ll_eval)
+    else:
+        assert False, "Something is wrong here"
+
+    ts, ms = ts[1:], ms[1:]  # Skip warmup step
+    mu_t, sigma_t = np.mean(ts).item(), np.std(ts).item()  # type: ignore[misc]
+    mu_m, sigma_m = np.mean(ms).item(), np.std(ms).item()  # type: ignore[misc]
+
+    print(f"Time (ms): {mu_t:.3f}+-{sigma_t:.3f}")
+    print(f"Memory (GiB): {mu_m:.3f}+-{sigma_m:.3f}")
+
+    return mu_t, sigma_t, mu_m, sigma_m
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset",        type=str,                   help="Unused, required for grid experiment")
+    parser.add_argument("--num-steps",      type=int,                   help="num_steps")
+    parser.add_argument("--batch-size",     type=int,                   help="batch_size")
+    parser.add_argument("--region-graph",   type=str,                   help="region_graph to use")
+    parser.add_argument("--layer",          type=str,                   help="Either 'cp', 'cp-shared' or 'tucker'")
+    parser.add_argument("--k",              type=int,   default=128,    help="Num categories for mixtures")
+    parser.add_argument("--gpu",            type=int,                   help="Which gpu to use")
+    parser.add_argument("--results-csv",    type=str,                   help="Path of the results csv (will be created if inexistent)")
+    args = parser.parse_args()
+    print(args)
+    device = torch.device(f"cuda:{args.gpu}") if args.gpu >= 0 else torch.device("cpu")
+
+    ######################################################################################
+    ################################## create fake fata ##################################
+    ######################################################################################
 
     num_vars = 28 * 28
-    data_size = args.batch_size if args.first_pass_only else args.num_batches * args.batch_size
+    data_size = args.num_steps * args.batch_size
     rand_data = torch.randint(256, (data_size, num_vars, 1), dtype=torch.float32)
     data_loader = DataLoader(
         dataset=TensorDataset(rand_data),
@@ -221,13 +200,17 @@ def do_benchmarking(args, mode: _Modes) -> None:
         drop_last=True,
     )
 
+    #######################################################################################
+    ################################## instantiate model ##################################
+    #######################################################################################
+
     # create RG
     REGION_GRAPHS = {
-    'QG':   QuadTree(width=28, height=28, struct_decomp=False),
-    'QT':   QuadTree(width=28, height=28, struct_decomp=True),
-    'PD':   PoonDomingos(shape=(28, 28), delta=4),
-    'CLT':  tree2rg(TREE_DICT["mnist"]),
-    'RQT':  RealQuadTree(width=28, height=28)
+        'QG':   QuadTree(width=28, height=28, struct_decomp=False),
+        'QT':   QuadTree(width=28, height=28, struct_decomp=True),
+        'PD':   PoonDomingos(shape=(28, 28), delta=4),
+        'CLT':  tree2rg(TREE_DICT["mnist"]),
+        'RQT':  RealQuadTree(width=28, height=28)
     }
 
     # choose layer
@@ -239,43 +222,17 @@ def do_benchmarking(args, mode: _Modes) -> None:
 
     assert args.region_graph in REGION_GRAPHS
     rg: RegionGraph = REGION_GRAPHS[args.region_graph]
-    layer = LAYER_TYPES[args.layer]
 
     pc = TensorizedPC.from_region_graph(
         rg,
-        layer_cls=layer,
+        layer_cls=LAYER_TYPES[args.layer],
         efamily_cls=CategoricalLayer,
         efamily_kwargs={"num_categories": 256},  # type: ignore[misc]
-        num_inner_units=args.num_latents,
-        num_input_units=args.num_latents,
-    )
-    pc.to(device)
-    print(pc)
-    print(f"Number of parameters: {sum(p.numel() for p in pc.parameters())}")
-
-    if mode == _Modes.TRAIN:
-        optimizer = optim.Adam(pc.parameters())  # just keep everything default
-        (ts, ms), ll_train = train(pc, optimizer, data_loader, device=device)
-        print("Train LL:", ll_train)
-    elif mode == _Modes.EVAL:
-        (ts, ms), ll_eval = evaluate(pc, data_loader, device=device)
-        print("Evaluation LL:", ll_eval)
-    else:
-        assert False, "Something is wrong here"
-    if not args.first_pass_only and args.num_batches > 1:
-        # Skip warmup step
-        ts, ms = ts[1:], ms[1:]
-    mu_t, sigma_t = np.mean(ts).item(), np.std(ts).item()  # type: ignore[misc]
-    mu_m, sigma_m = np.mean(ms).item(), np.std(ms).item()  # type: ignore[misc]
-
-    print(f"Time (ms): {mu_t:.3f}+-{sigma_t:.3f}")
-    print(f"Memory (MiB): {mu_m:.3f}+-{sigma_m:.3f}")
-
-    return mu_t, sigma_t, mu_m, sigma_m
-
-
-if __name__ == "__main__":
-    args = process_args()
+        num_inner_units=args.k,
+        num_input_units=args.k
+    ).to(device)
+    num_params = sum(p.numel() for p in pc.parameters())
+    print(f"Number of parameters: {num_params}")
 
     # benchmark evaluation mode
     eval_mu_t, eval_sigma_t, eval_mu_m, eval_sigma_m = do_benchmarking(args, mode="eval")
@@ -285,20 +242,20 @@ if __name__ == "__main__":
     csv_row = {
         "rg": args.region_graph,
         "layer": args.layer,
-        "k": args.num_latents,
-        "batch_size": args.batch_size,
-        "eval_time": eval_mu_t,
-        "eval_time_std": eval_sigma_t,
-        "eval_space": eval_mu_m,
-        "eval_space_std": eval_sigma_m,
-        "train_time": train_mu_t,
-        "train_time_std": train_sigma_t,
-        "train_space": train_mu_m,
-        "train_space_std": train_mu_t
+        "k": args.k,
+        "num-params": num_params,
+        "batch-size": args.batch_size,
+        "test-time": eval_mu_t,
+        "test-time-std": eval_sigma_t,
+        "test-space": eval_mu_m,
+        "test-space-std": eval_sigma_m,
+        "train-time": train_mu_t,
+        "train-time-std": train_sigma_t,
+        "train-space": train_mu_m,
+        "train-space-std": train_mu_t
     }
 
     df = pd.DataFrame.from_dict([csv_row])
-
     if os.path.exists(args.results_csv):
         df.to_csv(args.results_csv, mode="a", index=False)
     else:
