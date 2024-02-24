11450692dcd53133ef17ffdd5a1cc59655b515b1
anton <grasarade@gmail.com>
2024-02-24 15:58:59 +0100

    Bug fix

--- src/scripts/train.sh
+++ src/scripts/train.sh
@@ -1,4 +1,1 @@
-python src/training.py --dataset "mnist" \
-    --model-dir "out/" \
-    --lr 0.01 --num-sums 4  --rg "QG" --layer "cp" --leaf "cat" --reparam "softplus" \
-    --batch-size 128 --max-num-epochs 5 --progressbar True
+python src/training.py --dataset "mnist" --model-dir "out" --lr 0.01 --k 4  --rg "RQT" --layer "uncollapsed-cp" --input-type "cat" --reparam "clamp" --batch-size 128 --max-num-epochs 1 --progressbar True --rank 2
